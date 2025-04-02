8735a861bbd661eec1b3ced8c085f436ea14e383
Davide Passafaro <passafaro@hotmail.it>
2025-04-02 21:48:04 +0200

    feat: add counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   imports: [],
 })
-export class AppComponent {}
+export class AppComponent {
+  name = signal('Angular book');
+  image = signal(
+    'https://www.apogeonline.com/contrib/uploads/angular-copertina.jpg'
+  );
+  count = signal(0);
+
+  onDecrement() {
+    this.count.update((value) => value - 1);
+  }
+
+  onIncrement() {
+    this.count.update((value) => value + 1);
+  }
+
+  onReset() {
+    this.count.set(0);
+  }
+}
